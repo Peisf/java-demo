4a9e5e38283eafbae53ad0267a91f265489ea64a
帅飞 <shuaifei.pei@wanbangauto.com>
2022-01-19 20:31:45 +0800

    1

--- spring-mvc/src/main/java/com/itcast/controller/UserController.java
+++ spring-mvc/src/main/java/com/itcast/controller/UserController.java
@@ -1,14 +1,69 @@
 package com.itcast.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itcast.domain.User;
+import com.itcast.domain.VO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/quick")
     public String save(){
         System.out.println("Controller save running...");
         return "success.jsp";
+    }
+
+    @RequestMapping("/quick1")
+    @ResponseBody
+    public String save1() throws JsonProcessingException {
+        User user = new User();
+        user.setUsername("lisi");
+        user.setAge(18);
+
+        //使用json的转换工具将对象转换成json格式字符串在返回
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+        return json;
+    }
+
+    @RequestMapping("/quick2")
+    @ResponseBody
+    public User save2() {
+        User user = new User();
+        user.setUsername("lisi2");
+        user.setAge(18);
+        return user;
+    }
+
+    @RequestMapping("/quick3")
+    @ResponseBody
+    public void save3(String username, int age) {
+        System.out.println(username);
+        System.out.println(age);
+    }
+    @RequestMapping("/quick4")
+    @ResponseBody
+    public void save4(User user) {
+        System.out.println(user);
+    }
+
+    @RequestMapping("/quick5")
+    @ResponseBody
+    public void save5(String[] strings) {
+        System.out.println(Arrays.asList(strings));
+    }
+
+    @RequestMapping("/quick6")
+    @ResponseBody
+    public void save6(VO vo) {
+        System.out.println(vo);
     }
 }
---
+++ spring-mvc/src/main/webapp/form.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: SC4029
+  Date: 2022/1/19
+  Time: 16:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="user/quick6" method="post">
+    <%--表明是第几个User对象的username age--%>
+    <input type="text" name="userList[0].username"><br/>
+    <input type="text" name="userList[0].age"><br/>
+    <input type="text" name="userList[1].username"><br/>
+    <input type="text" name="userList[1].age"><br/>
+    <input type="text" name="userList[2].username"><br/>
+    <input type="text" name="userList[2].age"><br/>
+    <input type="submit" value="提交">
+</form>
+</body>
+</html>
